1117f27f15304de26d82244a9ea47f347915ae36
Tonny Auma <tonny.auma@interswitchgroup.com>
2021-08-18 20:58:35 +0300

    sipatec

--- src/SipatecSyncfusion/SipatecWpfSyncfusion/MainWindow.xaml.cs
+++ src/SipatecSyncfusion/SipatecWpfSyncfusion/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,17 +11,107 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Syncfusion.SfSkinManager;
+using Syncfusion.UI.Xaml.Diagram.Controls;
 
-namespace SipatecWpfSyncfusion
+namespace SIPATEC_POC
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow : Window
-  {
-    public MainWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-      InitializeComponent();
+        #region Fields
+        private string currentVisualStyle;
+        private string currentSizeMode;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets the current visual style.
+        /// </summary>
+        /// <value></value>
+        /// <remarks></remarks>
+        public string CurrentVisualStyle
+        {
+            get
+            {
+                return currentVisualStyle;
+            }
+            set
+            {
+                currentVisualStyle = value;
+                OnVisualStyleChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the current Size mode.
+        /// </summary>
+        /// <value></value>
+        /// <remarks></remarks>
+        public string CurrentSizeMode
+        {
+            get
+            {
+                return currentSizeMode;
+            }
+            set
+            {
+                currentSizeMode = value;
+                OnSizeModeChanged();
+            }
+        }
+        #endregion
+        public MainWindow()
+        {
+            InitializeComponent();
+            //this.Loaded += OnLoaded;
+            diagram.HorizontalRuler = new Ruler();
+            diagram.VerticalRuler = new Ruler() { Orientation = Orientation.Vertical };
+
+
+        }
+        /// <summary>
+        /// Called when [loaded].
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            CurrentVisualStyle = "FluentLight";
+            CurrentSizeMode = "Default";
+        }
+        /// <summary>
+        /// On Visual Style Changed.
+        /// </summary>
+        /// <remarks></remarks>
+        private void OnVisualStyleChanged()
+        {
+            VisualStyles visualStyle = VisualStyles.Default;
+            Enum.TryParse(CurrentVisualStyle, out visualStyle);
+            if (visualStyle != VisualStyles.Default)
+            {
+                SfSkinManager.ApplyStylesOnApplication = true;
+                SfSkinManager.SetVisualStyle(this, visualStyle);
+                SfSkinManager.ApplyStylesOnApplication = false;
+            }
+        }
+
+        /// <summary>
+        /// On Size Mode Changed event.
+        /// </summary>
+        /// <remarks></remarks>
+        private void OnSizeModeChanged()
+        {
+            SizeMode sizeMode = SizeMode.Default;
+            Enum.TryParse(CurrentSizeMode, out sizeMode);
+            if (sizeMode != SizeMode.Default)
+            {
+                SfSkinManager.ApplyStylesOnApplication = true;
+                SfSkinManager.SetSizeMode(this, sizeMode);
+                SfSkinManager.ApplyStylesOnApplication = false;
+            }
+        }
     }
-  }
 }
